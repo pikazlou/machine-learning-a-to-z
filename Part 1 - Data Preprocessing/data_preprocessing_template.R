4a023a9c1f6c09b7d34549395f9e7fa3d08c0f8f
Pavel Kazlou <p.kazlou@adform.com>
2018-01-20 17:50:13 +0300

    Part 1: preparing data in R

--- Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -3,6 +3,24 @@
 # Importing the dataset
 dataset = read.csv('Data.csv')
 
+# Taking care of missing data
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+
+
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
+
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1, 2, 3))
+
+dataset$Purchased = factor(dataset$Purchased,
+                           levels = c('Yes', 'No'),
+                           labels = c(1, 0))
+
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
 library(caTools)
